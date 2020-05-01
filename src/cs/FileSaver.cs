45e6745799fd6807bd97ae8b34f5db6f9a56f836
VACEfron <46462862+VACEfron@users.noreply.github.com>
2020-05-01 16:46:51 +0200

    Version 2
    Renamed to 'Image Collector', added categories, cleaned up files & code.

---
+++ src/cs/FileSaver.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Image_Collector
+{
+    public class FileSaver
+    {
+        public static void SaveFile(string Type)
+        {
+            string subredditsJson = new StreamReader(Directory.GetCurrentDirectory() + $"/subreddits.json").ReadToEnd();
+            var json = (JObject)JsonConvert.DeserializeObject(subredditsJson);
+            string[] Subreddits = JsonConvert.DeserializeObject<List<string>>(json[Type].ToString()).ToArray();
+
+            Console.WriteLine($">> Image Collector by VAC Efron\n\n{Type} will be saved in {Directory.GetCurrentDirectory() + $@"\{Type}\"}\n");
+
+            while (true)
+            {
+                try
+                {
+                    Task.Delay(TimeSpan.FromSeconds(1));
+                    Random r = new Random();
+
+                    string[] SubSections = { "new.json", "rising.json", "top/.json?t=day", "top/.json?t=week", "top/.json?t=month" };
+                    var Subname = Subreddits[r.Next(Subreddits.Length)];
+                    var SubSection = SubSections[r.Next(SubSections.Length)];
+
+                    string RequestUrl = $"https://www.reddit.com/r/{Subname}/{SubSection}";
+
+                    var request = (HttpWebRequest)WebRequest.Create(RequestUrl);
+                    var response = (HttpWebResponse)request.GetResponse();
+                    var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                    var data = (JObject)JsonConvert.DeserializeObject(responseString);
+                    int postscount = data["data"]["dist"].Value<int>();
+                    if (postscount is 0) continue;
+                    string url = data["data"]["children"][r.Next(0, postscount)]["data"]["url"].Value<string>();
+
+                    using (var WebClient = new WebClient())
+                    {
+                        byte[] Byte = WebClient.DownloadData(url);
+                        var stream = new MemoryStream(Byte);
+                        var path = Directory.GetCurrentDirectory() + $@"\{Type}\";
+
+                        string filename = Regex.Match(url, @"\/([A-Za-z0-9\-._~:?#\[\]@!$%&'()*+,;=]*)(.jpg|.JPG|.jpeg|.JPEG|.png|.PNG)").Groups[1].Value + ".png";
+                        if (filename == ".png" || filename is null) continue;
+                        
+                        SaveStreamAsFile(path, stream, filename);
+                    }
+                }
+                catch { continue; }
+            }
+        }
+
+        public static void SaveStreamAsFile(string filePath, Stream inputStream, string fileName)
+        {
+            DirectoryInfo info = new DirectoryInfo(filePath);
+            if (!info.Exists)           
+                info.Create();        
+
+            string path = Path.Combine(filePath, fileName);
+            if (File.Exists(path)) return;
+
+            using (FileStream outputFileStream = new FileStream(path, FileMode.Create))
+            {
+                inputStream.CopyTo(outputFileStream);
+                Console.WriteLine($"Saved file: {fileName} | Total images saved: {Directory.GetFiles(filePath).Length}");
+            }
+        }
+    }
+}
---
+++ src/cs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Image_Collector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Categories = { "memes", "cats", "dogs", "birds", "battlestations" };
+            string ChooseCategory = string.Join("\n", Categories.Select(x => $" - {x}"));
+
+            Console.WriteLine(
+                ">> Image Collector by VAC Efron\n" +
+                "Automatically save Images from various Reddit communities.\n\n" +
+                "Disclaimer: Reddit proves a limited amount of posts, meaning that eventually there will be no more memes to save. You'll have to wait until new posts appear on the subreddits.\n\n" +
+                ">> Discord: https://discord.gg/TtR32WT");
+        Read:
+            Console.WriteLine($"\nChoose a category:\n{ChooseCategory}");
+            var read = Console.ReadLine().ToLower();
+            if (Categories.Any(x => x == read))
+            {
+                Console.Clear();
+                FileSaver.SaveFile(char.ToUpper(read[0]) + read.Substring(1));
+            }
+            else goto Read;
+        }
+    }
+}
